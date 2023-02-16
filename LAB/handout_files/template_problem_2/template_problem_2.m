75b71e24bef2116198e274ca760d24d8fec5ad38
olakrhoff <ola.kr@hotmail.no>
2023-02-16 09:51:29 +0100

    Feat: update ignore and add files

--- LAB/handout_files/template_problem_2/template_problem_2.m
+++ LAB/handout_files/template_problem_2/template_problem_2.m
@@ -1,35 +1,38 @@
 % TTK4135 - Helicopter lab
 % Hints/template for problem 2.
-% Updated spring 2018, Andreas L. Flåten
+% Updated spring 2018, Andreas L. Flï¿½ten
 
 %% Initialization and model definition
-initXX; % Change this to the init file corresponding to your helicopter
+init03; % Change this to the init file corresponding to your helicopter
 
 % Discrete time system model. x = [lambda r p p_dot]'
 delta_t	= 0.25; % sampling time
-A1 = [];
-B1 = [];
+A1 = [1 delta_t 0 0;
+      0 1 -delta_t * K_2 0;
+      0 0 1 delta_t;
+      0 0 -K_1*K_pp*delta_t 1-K_1*K_pd*delta_t];
+B1 = [0; 0; 0; delta_t*K_1*K_pp];
 
 % Number of states and inputs
 mx = size(A1,2); % Number of states (number of columns in A)
 mu = size(B1,2); % Number of inputs(number of columns in B)
 
 % Initial values
-x1_0 = ;                               % Lambda
-x2_0 = ;                               % r
-x3_0 = ;                               % p
-x4_0 = ;                               % p_dot
+x1_0 = pi;                               % Lambda
+x2_0 = 0;                               % r
+x3_0 = 0;                               % p
+x4_0 = 0;                               % p_dot
 x0 = [x1_0 x2_0 x3_0 x4_0]';           % Initial values
 
 % Time horizon and initialization
-N  = ;                                  % Time horizon for states
+N  = 100;                                  % Time horizon for states
 M  = N;                                 % Time horizon for inputs
 z  = zeros(N*mx+M*mu,1);                % Initialize z for the whole horizon
 z0 = z;                                 % Initial value for optimization
 
 % Bounds
-ul 	    = ;                   % Lower bound on control
-uu 	    = ;                   % Upper bound on control
+ul 	    = -pi/6;                   % Lower bound on control
+uu 	    = pi/6;                   % Upper bound on control
 
 xl      = -Inf*ones(mx,1);              % Lower bound on states (no bound)
 xu      = Inf*ones(mx,1);               % Upper bound on states (no bound)
@@ -37,27 +40,27 @@ xl(3)   = ul;                           % Lower bound on state x3
 xu(3)   = uu;                           % Upper bound on state x3
 
 % Generate constraints on measurements and inputs
-[vlb,vub]       = ; % hint: gen_constraints
+[vlb,vub]       = gen_constraints(N,M,xl,xu,ul,uu); % hint: gen_constraints
 vlb(N*mx+M*mu)  = 0;                    % We want the last input to be zero
 vub(N*mx+M*mu)  = 0;                    % We want the last input to be zero
 
 % Generate the matrix Q and the vector c (objecitve function weights in the QP problem) 
 Q1 = zeros(mx,mx);
-Q1(1,1) = ;                            % Weight on state x1
-Q1(2,2) = ;                            % Weight on state x2
-Q1(3,3) = ;                            % Weight on state x3
-Q1(4,4) = ;                            % Weight on state x4
+Q1(1,1) = 0.12;                            % Weight on state x1
+Q1(2,2) = 0.12;                            % Weight on state x2
+Q1(3,3) = 0.12;                            % Weight on state x3
+Q1(4,4) = 0.12;                            % Weight on state x4
 P1 = 0;                                % Weight on input
-Q = ;                                  % Generate Q, hint: gen_q
-c = ;                                  % Generate c, this is the linear constant term in the QP
+Q = gen_q(Q1,P1,N,M);                                  % Generate Q, hint: gen_q
+c = 0;                                  % Generate c, this is the linear constant term in the QP
 
 %% Generate system matrixes for linear model
-Aeq = ;             % Generate A, hint: gen_aeq
-beq = ;             % Generate b
-
+Aeq = gen_aeq(A1,B1,N,mx,mu);             % Generate A, hint: gen_aeq
+beq = zeros(1, 400);                      % Generate b
+beq(1,1) = pi;  
 %% Solve QP problem with linear model
 tic
-[z,lambda] = ; % hint: quadprog. Type 'doc quadprog' for more info 
+[z,lambda] = quadprog(Q, [], [], [], Aeq, beq, vlb, vub); % hint: quadprog. Type 'doc quadprog' for more info 
 t1=toc;
 
 % Calculate objective value
